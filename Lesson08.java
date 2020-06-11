76cd73c0e862e5ccbd374936f17d2c5880f8949c
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-11 18:12:42 +0900

    配列について学習

---
+++ Lesson08.java
@@ -0,0 +1,14 @@
+class Lesson08 {
+    public static void main(String[] args){
+        String[] arr;
+        arr = new String[3];
+
+        arr[0]="sato";
+        arr[1]="suzuki";
+        arr[2]="takahashi";
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }    
+}
---
+++ Lesson09.java
@@ -0,0 +1,8 @@
+class Lesson09 {
+    public static void main(String[] args){
+        int x = 8;
+        int y = 3;
+        System.out.println(x==3 || y==3);
+    }
+
+}
