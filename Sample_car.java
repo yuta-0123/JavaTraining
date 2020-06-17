1d250e6ecb66607a46da9734bbb71eb297a6717f
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-17 12:55:22 +0900

    コメントを追加しながらコンストラクタについて学習

--- Sample_car.java
+++ Sample_car.java
@@ -1,5 +1,7 @@
 class Sample_car {
 
+    // Sample_carクラスの役割：変数やコンストラクタを定義する為
+
     // フィールド(クラス直下で定義された変数)
     //クラスの時点は初期化なしで定義。
     // 変数に何も入っていない状況。（＝実体 例:車という概念) 
@@ -22,22 +24,29 @@ class Sample_car {
     // メソッド名はクラス名と同じ
     // 戻り値なし
 
-    // パターンに対応できる様に、コンストラクタを定義しておく。
+    // パターンに対応できる様に、コンストラクタをオートロードして、定義しておく。
 
     // パターン１：引数無し
+    // フィールドの値を未登録のものを対応
+
     Sample_car(){
         System.out.println("constructor");
-        carModel = "zero";
-        owner = "zero";
-        color = "zero";
+
+        //変数の準備と代入処理
+
+        carModel = "未登録";
+        owner = "未登録";
+        color = "未登録";
         speed = 0;
         right = false;
         System.out.println("=====================================");
+        // フィールドの代入が終わった時が、インスタンス化の終了
     }
 
     
     // パターン１：引数有り
-    // 好きな値をフィールドに詰める。
+    // 好きな値をフィールドに詰める
+
     Sample_car(String cm ,String on,String cl, int sp ,boolean rt){
         System.out.println("constructor");
         carModel = cm;
--- Sample_drive.java
+++ Sample_drive.java
@@ -1,15 +1,20 @@
 class Sample_drive {
 
+    // Sample_driveの役割：インスタンス化する為
+
     public static void main (String[] args){
         // 1:クラスをインスタンス化
-        System.out.println("======================");
         
         // インスタンス化の右辺はコンストラクタを表している。
         // クラス名＋( )
         
+        // コンストラクタをインスタンス化していく。
+
         // パターン１：引数無し
         Sample_car chocoCar = new Sample_car();
-        // パターン２：引数５つのもの
+
+        // パターン２：引数あり
+        // ５つのデータをいれたもの
         Sample_car mocoCar = new Sample_car("aaa","bbb","RED",100,true);
         Sample_car pochiCar = new Sample_car("koko","zzz","BLUE",80,true);
 
