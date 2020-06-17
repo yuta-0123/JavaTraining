29bacb32996eaa21e105236ce631a0dd50d0cc5b
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-17 12:29:13 +0900

    コメントを追加しながら学習

--- Sample_car.java
+++ Sample_car.java
@@ -1,22 +1,45 @@
 class Sample_car {
+
+    // フィールド(クラス直下で定義された変数)
+    //クラスの時点は初期化なしで定義。
+    // 変数に何も入っていない状況。（＝実体 例:車という概念) 
+
     String carModel;
     String owner;
     String color;
     int speed;
     boolean right;
 
+    // メソッド
+    // accelerateメソッド
+    void accelerator(){
+        speed++;
+        System.out.println("speed up");
+    }
+
+    // コンストラクタ(インスタンス化と同時に生成。)
+    // フィールドの初期値を設定。
+    // メソッド名はクラス名と同じ
+    // 戻り値なし
+
+    // パターンに対応できる様に、コンストラクタを定義しておく。
+
+    // パターン１：引数無し
     Sample_car(){
-        System.out.println("=====================================");
-        carModel = "aaa";
-        owner = "aaa";
-        color = "aaa";
+        System.out.println("constructor");
+        carModel = "zero";
+        owner = "zero";
+        color = "zero";
         speed = 0;
         right = false;
         System.out.println("=====================================");
     }
 
+    
+    // パターン１：引数有り
+    // 好きな値をフィールドに詰める。
     Sample_car(String cm ,String on,String cl, int sp ,boolean rt){
-        System.out.println("=====================================");
+        System.out.println("constructor");
         carModel = cm;
         owner = on;
         color = cl;
--- Sample_drive.java
+++ Sample_drive.java
@@ -1,13 +1,20 @@
 class Sample_drive {
-    
-    public static void main (String[] args){
-        System.out.println("======================");
 
+    public static void main (String[] args){
+        // 1:クラスをインスタンス化
+        System.out.println("======================");
+        
+        // インスタンス化の右辺はコンストラクタを表している。
+        // クラス名＋( )
+        
+        // パターン１：引数無し
         Sample_car chocoCar = new Sample_car();
+        // パターン２：引数５つのもの
         Sample_car mocoCar = new Sample_car("aaa","bbb","RED",100,true);
         Sample_car pochiCar = new Sample_car("koko","zzz","BLUE",80,true);
 
         System.out.println("=======================");
+        // 2:インスタンスのフィールドの確認
         System.out.println("chocoCar.carModel : "+ chocoCar.carModel);
         System.out.println("chocoCar.owner : "+ chocoCar.owner);
         System.out.println("=======================");
