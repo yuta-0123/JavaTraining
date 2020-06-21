09f43f03eb4c05fd5f41f419779df24b06f0eff3
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-21 11:01:51 +0900

    カプセル化 について学習

--- Sample_drive02.java
+++ Sample_drive02.java
@@ -10,6 +10,9 @@ class Sample_drive02 {
         Sample_car02 mocoCar = new Sample_car02("aaa","bbb","RED",100,true);
         Sample_car02 pochiCar = new Sample_car02("koko","zzz","BLUE",80,true);
 
+        // ↑コンストラクタがないのに、コンストラクタを呼び出している。
+        // ↑デフォルトコンストラクタと呼ぶ。
+
         System.out.println("=======================");
         // 2:インスタンスのフィールドの確認
         System.out.println("chocoCar.carModel : "+ chocoCar.carModel);
